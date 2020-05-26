599fcb7ef9225a6011ea2498e3cf9c4281efcd8f
Jonas Duri <jonas.duri@gmail.com>
2020-05-26 19:03:54 +0200

    Use API prefix instead of subdomain

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -72,7 +72,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::domain('api.thing.gs.test')
+        Route::prefix('api')
+            ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
         /*
--- tests/Feature/ThingTest.php
+++ tests/Feature/ThingTest.php
@@ -15,7 +15,7 @@ class ThingTest extends TestCase
     public function a_user_can_send_an_url_to_the_api()
     {
         $this->withoutExceptionHandling();
-        $response = $this->get('http://api.' . env('APP_DOMAIN') . '/create?url=http://google.com');
+        $response = $this->get('/api/create?url=http://google.com');
         $response->assertStatus(200);
         $response->assertJsonFragment([
             'source_url' => 'http://google.com'
@@ -28,7 +28,7 @@ class ThingTest extends TestCase
     public function a_user_sees_an_error_when_url_is_invalid()
     {
         $this->withoutExceptionHandling();
-        $response = $this->get('http://api.' . env('APP_DOMAIN') . '/create?url=invalid');
+        $response = $this->get('/api/create?url=invalid');
         $response->assertStatus(400);
         $response->assertJsonFragment([
             'url' => [
@@ -43,7 +43,7 @@ class ThingTest extends TestCase
     public function a_user_sees_an_error_when_url_is_missing()
     {
         $this->withoutExceptionHandling();
-        $response = $this->get('http://api.' . env('APP_DOMAIN') . '/create');
+        $response = $this->get('/api/create');
         $response->assertStatus(400);
         $response->assertJsonFragment([
             'url' => [
@@ -58,12 +58,11 @@ class ThingTest extends TestCase
     public function a_user_can_create_a_short_url()
     {
         $this->withoutExceptionHandling();
-        $response = $this->get('http://api.' . env('APP_DOMAIN') . '/create?url=http://google.com');
+        $response = $this->get('/api/create?url=http://google.com');
         $response->assertStatus(200);
         $response->assertJsonFragment([
             'source_url' => 'http://google.com',
-            'short_code' => 'cJinsNv1',
-            'short_url' => 'http://' . env('APP_DOMAIN') . '/cJinsNv1'
+            'short_code' => 'cJinsNv1'
         ]);
     }
 }
